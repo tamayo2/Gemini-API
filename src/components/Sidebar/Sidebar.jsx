b1d707e0262c9912fc87c5fb6c514f6b292a47b4
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-04 23:42:53 -0500

    Subiendo actualizaciones Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,28 @@
 import './Sidebar.css'
 import {assets} from "../../assets/assets.js";
+import {useState} from "react";
 
 const Sidebar = () => {
+
+    const [extended, setExtended] = useState(false);
+
     return (
         <div className= 'sidebar'>
             <div className= 'top'>
                 <img className= 'menu' src={assets.menu_icon} alt= '' />
                 <div className= 'new-chat'>
                     <img src={assets.plus_icon} alt=''/>
-                    <p>New Chat</p>
+                    {extended?<p>New Chat</p>:null}
                 </div>
+                {extended?
+                    <div className='recent'>
+                        <p className='recent-title'>Recent</p>
+                        <div className='recent-entry'>
+                            <img src={assets.message_icon} alt=''/>
+                            <p>What is react...</p>
+                        </div>
+                    </div>
+                }
                 <div className='recent'>
                     <p className='recent-title'>Recent</p>
                     <div className='recent-entry'>
