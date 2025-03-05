3b4a6ce1c11970217fba780045e69263151f2c29
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-05 01:20:11 -0500

    Actualizacion cards principales

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,39 @@
+import './Main.css'
+import {assets} from "../../assets/assets.js";
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className='nav'>
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt='asset' />
+            </div>
+            <div className='main-container' >
+                <div className='greet'>
+                    <p><span>Hello, Dev</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className= "cards">
+                    <div className='card'>
+                        <p>Suggest beatiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt='compass' />
+                    </div>
+                    <div className='card'>
+                        <p>Suggest beatiful places to see on an upcoming road trip</p>
+                        <img src={assets.bulb_icon} alt='compass' />
+                    </div>
+                    <div className='card'>
+                        <p>Suggest beatiful places to see on an upcoming road trip</p>
+                        <img src={assets.message_icon} alt='compass' />
+                    </div>
+                    <div className='card'>
+                        <p>Suggest beatiful places to see on an upcoming road trip</p>
+                        <img src={assets.code_icon} alt='compass' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
