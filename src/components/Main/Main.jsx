1295e49624fa2fc019ee08bdc9ac24b58df9054d
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-06 01:51:05 -0500

    Finalizando Context

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
 import './Main.css'
 import {assets} from "../../assets/assets.js";
+import {useContext} from "react";
+import {Context} from "../../context/Context.jsx";
 
 const Main = () => {
+
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
+
     return (
         <div className='main'>
             <div className='nav'>
@@ -9,35 +14,57 @@ const Main = () => {
                 <img src={assets.user_icon} alt='asset' />
             </div>
             <div className='main-container' >
-                <div className='greet'>
-                    <p><span>Hello, Dev</span></p>
-                    <p>How can I help you today?</p>
-                </div>
-                <div className= "cards">
-                    <div className='card'>
-                        <p>Suggest beatiful places to see on an upcoming road trip</p>
-                        <img src={assets.compass_icon} alt='compass' />
+                {!showResult
+                ?<>
+                        <div className='greet'>
+                            <p><span>Hello, Dev</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                        <div className= "cards">
+                            <div className='card'>
+                                <p>Suggest beatiful places to see on an upcoming road trip</p>
+                                <img src={assets.compass_icon} alt='compass' />
+                            </div>
+                            <div className='card'>
+                                <p>Suggest beatiful places to see on an upcoming road trip</p>
+                                <img src={assets.bulb_icon} alt='compass' />
+                            </div>
+                            <div className='card'>
+                                <p>Suggest beatiful places to see on an upcoming road trip</p>
+                                <img src={assets.message_icon} alt='compass' />
+                            </div>
+                            <div className='card'>
+                                <p>Suggest beatiful places to see on an upcoming road trip</p>
+                                <img src={assets.code_icon} alt='compass' />
+                            </div>
+                        </div>
+                </>
+                    :<div className="result">
+                        <div className='result-title'>
+                            <img src={assets.user_icon} alt='user' />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className='result-data'>
+                            <img src={assets.gemini_icon} alt='compass' />
+                            {loading
+                            ? <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                            </div>
+                                :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                            }
+                        </div>
                     </div>
-                    <div className='card'>
-                        <p>Suggest beatiful places to see on an upcoming road trip</p>
-                        <img src={assets.bulb_icon} alt='compass' />
-                    </div>
-                    <div className='card'>
-                        <p>Suggest beatiful places to see on an upcoming road trip</p>
-                        <img src={assets.message_icon} alt='compass' />
-                    </div>
-                    <div className='card'>
-                        <p>Suggest beatiful places to see on an upcoming road trip</p>
-                        <img src={assets.code_icon} alt='compass' />
-                    </div>
-                </div>
+                }
+
                 <div className='main-bottom'>
                     <div className='search-box'>
-                        <input type='text' placeholder='Enter a prompt here' />
+                        <input onChange={(e)=>setInput(e.target.value)} value={input}  type='text' placeholder='Enter a prompt here' />
                         <div>
                             <img src={assets.gallery_icon} alt='gallery' />
                             <img src={assets.mic_icon} alt='gallery'/>
-                            <img src={assets.send_icon} alt='gallery' />
+                            <img onClick={() => onSent ()} src={assets.send_icon} alt='gallery' />
                         </div>
                     </div>
                     <p className='bottom-info '>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -8,12 +8,21 @@ const ContextProvider = (props) => {
     const [input, setInput] = useState("");
     const [recentPrompt, setRecentPrompt] = useState("");
     const [prevPrompts, setPrevPrompts] = useState([]);
-    const [showResult, setResult] = useState(false);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
     const onSent = async (prompt) => {
-        await run(prompt)
+
+        setResultData("")
+        setLoading(true);
+        setShowResult(true);
+        setRecentPrompt(input)
+        const response = await run(input)
+        setResultData(response)
+        setLoading(false);
+        setInput("");
+        setInput("")
     }
 
     const contextValue = {
